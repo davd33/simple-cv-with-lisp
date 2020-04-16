6bcc80c0daabadb5d89e3ee0b8439957b076b309
David Rueda <davd33@gmail.com>
2020-04-16 21:34:48 +0200

    tests: (ok (not ...)) = (ng ...)

--- tests/jsons.lisp
+++ tests/jsons.lisp
@@ -51,14 +51,14 @@
   (testing "all compatible"
     (ok (type-comp-acar? acar-ok)))
   (testing "depth-1 field doesn't match slot name"
-    (ok (not (type-comp-acar? "{\"siz\":12,\"wheel-list\":[{\"radius\":10}]},\"color\":{\"r\":12,\"g\":233,\"b\":2}"))))
+    (ng (type-comp-acar? "{\"siz\":12,\"wheel-list\":[{\"radius\":10}]},\"color\":{\"r\":12,\"g\":233,\"b\":2}")))
   (testing "depth-2 field doesn't match slot name"
-    (ok (not (type-comp-acar? "{\"size\":12,\"wheel-list\":[{\"radiu\":10}]},\"color\":{\"r\":12,\"g\":233,\"b\":2}"))))
+    (ng (type-comp-acar? "{\"size\":12,\"wheel-list\":[{\"radiu\":10}]},\"color\":{\"r\":12,\"g\":233,\"b\":2}")))
   (testing "depth-2 field should have been an array"
-    (ok (not (type-comp-acar? "{\"size\":12,\"wheel-list\":{\"radius\":10},\"color\":{\"r\":12,\"g\":233,\"b\":2}}"))))
+    (ng (type-comp-acar? "{\"size\":12,\"wheel-list\":{\"radius\":10},\"color\":{\"r\":12,\"g\":233,\"b\":2}}")))
   (testing "a JSON object could simply be an array of objects!"
     (ok (type-comp-acar[]? acar-array-ok)))
   (testing "this json should have been an array!"
-    (ok (not (type-comp-acar[]? "{\"size\":12,\"wheel-list\":[{\"radius\":10}],\"color\":{\"r\":12,\"g\":233,\"b\":2}}"))))
+    (ng (type-comp-acar[]? "{\"size\":12,\"wheel-list\":[{\"radius\":10}],\"color\":{\"r\":12,\"g\":233,\"b\":2}}")))
   (testing "it's ok to miss an optional field (e.g. field-o)"
     (ok (type-comp-acar? "{\"size\":12,\"wheel-list\":[{\"radius\":10}],\"color\":{\"r\":12,\"g\":233,\"b\":2}}"))))
